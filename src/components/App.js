854447e1b86ef188096c0539825284b8e1062250
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-11-05 08:10:28 +0000

    x

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./render";
 
+let arr = [{title:"Tab 1"},{title:"Tab 2"}]
 const App = () => {
+  console.log(arr)
   return (
+    
     <div>
-        {/* Do not remove the main div */}
+      <h1>Tabs Example</h1>
+      <Tabs tabs={arr} />
     </div>
-  )
+  );
+
+  
 }
 
 export default App
---
+++ src/components/render.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Tabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState('');
+
+  function handleClick(tab){
+    setActiveTab(tab)
+  }
+
+  return (
+    <div>
+      <ul>
+        {
+          tabs.map((tab)=>{
+            return (
+              <li onClick={()=>{
+                handleClick(tab.title)
+              }}>{tab.title}</li>
+            )
+          })
+
+        }
+      </ul>
+      <p>This is the content for {activeTab}</p>
+    </div>
+  )
+  
+}
+
+export default Tabs;
